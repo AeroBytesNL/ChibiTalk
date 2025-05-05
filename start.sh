da94079515f900b0202cf46238676da7efe179a1
AeroBytesNL <kelvin@aerobytes.nl>
2025-05-05 12:48:52 +0200

    Updated docker compose

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+until nc -z "$DB_HOST" "$DB_PORT"; do
+  echo "Waiting for DB ($DB_HOST:$DB_PORT)..."
+  sleep 2
+done
+
+php artisan migrate --force
+
+npm run build
+
+php artisan reverb:start &
+
