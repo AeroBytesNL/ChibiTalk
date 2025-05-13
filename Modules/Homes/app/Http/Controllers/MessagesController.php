ac9d1896be5111789f43a844667bb12bb954353a
AeroBytesNL <kelvin@aerobytes.nl>
2025-05-13 16:37:03 +0200

    Added message deletion

--- Modules/Homes/app/Http/Controllers/MessagesController.php
+++ Modules/Homes/app/Http/Controllers/MessagesController.php
@@ -4,53 +4,45 @@ namespace Modules\Homes\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Modules\Homes\Models\Message;
 
 class MessagesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         return view('homes::index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
         return view('homes::create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request) {}
 
-    /**
-     * Show the specified resource.
-     */
     public function show($id)
     {
         return view('homes::show');
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit($id)
     {
         return view('homes::edit');
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, $id) {}
 
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy($id) {}
+    public function destroy($id)
+    {
+        $message = Message::findOrFail($id);
+
+        // TODO: When RBAC is implemented, make this compatible
+        if ($message->user_id !== Auth::id()) {
+            return redirect()->back()->with('error', 'Not allowed to delete this message.');
+        }
+
+        $message->delete();
+        return redirect()->back()->with('success', 'Message deleted.');
+    }
 }
